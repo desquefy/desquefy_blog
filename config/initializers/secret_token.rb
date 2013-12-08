1d84457cbc20afff2cafad14751b78b196c54d0d
Apple <Apple@AppletekiMacBook-Air.local>
2013-12-08 10:49:08 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DesquefyBlog::Application.config.secret_key_base = '8fb5aa5b33a97c3b99e58007f3c74b2afb91ac69c30daa3e918310cf0a669da413e8c77e0689d3170f01dcec76688d5009d0c85d30e3f6f032148e3e75139314'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DesquefyBlog::Application.config.session_store :cookie_store, key: '_desquefy_blog_session'
